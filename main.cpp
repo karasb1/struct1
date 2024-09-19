7596d16ea17c7a8952dcf9821a5d7c84e310f7e5
karasb1 <bohdankaras456@gmail.com>
2024-09-19 20:07:22 +0300

    The ex2 was done.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+struct MyStruct
+{
+	int x;
+	int y;
+
+};
+void distance(MyStruct& pointA, MyStruct& pointB, int result = 0)
+{
+	result = sqrt(pow((pointA.x - pointB.x), 2) + pow((pointA.y - pointB.y), 2));
+	cout << result << endl;
+}
 
 int main()
 {
-	int a;
+	MyStruct pointA = {5, 5 };
+	MyStruct pointB = { 5, 10 };
+	distance(pointA, pointB);
+	
 
 	return 0;
 }
